4632064bafb4eb3c2346e8719595831be72a7eb6
IslamAhmed0 <ea43533@gmail.com>
2021-05-17 16:58:14 +0200

    productRoute

--- models/product.js
+++ models/product.js
@@ -1,12 +1,33 @@
 const mongoose=require('mongoose');
 
 const prodctSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    image: { type: String, required: true },
-    brand: { type: String, required: true },
-    price: { type: Number, default: 0, required: true },
-    category: { type: String, required: true },
-    description: { type: String, required: true },
+    name:
+        {
+            type: String,
+            required: true
+        },
+    image:
+        { type: String,
+            required: true
+        },
+    brand:
+        { type: String,
+            required: true
+        },
+    price:
+        { type: Number,
+            default: 0,
+            required: true
+        },
+    category:
+        { type: String,
+            required: true
+        },
+    description:
+        {
+            type: String,
+            required: true
+        },
 });
 
 const Product = mongoose.model('Product', prodctSchema);
