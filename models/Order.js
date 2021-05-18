f7d87bcb4c24631fad5203b9d6c0856419c851d1
Alshimaa safwat <shimaa.safwat112@gmail.com>
2021-05-18 03:49:22 +0200

    add order model

---
+++ models/Order.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const {Schema} = require("mongoose");
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    totalPrice:{
+        type: Number,
+        default: 0,
+        required: true
+    },
+ 
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+        required:true
+    }],
+
+    status: {
+        type: String,
+        enum: ['pending','accepted','rejected'],
+        required: true
+    },
+
+    timestamps: { 
+        createdAt: 'created_at', 
+        updatedAt:  false 
+    },
+
+});
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
