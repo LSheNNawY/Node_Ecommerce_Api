2c60a89d27cb7608267fc61ede5584919cf1a452
Alshimaa safwat <shimaa.safwat112@gmail.com>
2021-05-18 20:26:04 +0200

    update/getbyid order

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -24,6 +24,35 @@ const createOrder = async(req,res)=>{
 }
 
 
+const getById = async(req,res)=>{
+    try {
+    const order= await Order.findOne({ _id: req.params.id });
+    if (order) {
+        res.send(order);
+    } 
+}catch(err){
+    return res.status(400).send({"message":"order not found"});
+ }
+}
+const getByIdAndUpdate = async(req,res)=>{
+    try {
+    const order =await Order.findOneAndUpdate(
+        req.params.id,
+        {
+            status:req.body.status
+        }
+    );
+    if (order) {
+        res.send(order);
+    } 
+}catch(err){
+    return res.status(400).send({"message":"order not found"});
+ }
+
+    
+}
+
+
 module.exports={
-    getAll,createOrder
+    getAll,createOrder, getById, getByIdAndUpdate
 }
