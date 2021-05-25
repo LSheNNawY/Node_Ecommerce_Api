c63e7f35660d0cde712fbc638c8488ae0b0f1f47
Muhammed Shendi <muhammedshendi@Muhammeds-MacBook-Pro.local>
2021-05-25 04:00:52 +0200

    dashboard product creation

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(cors({
 }))
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
