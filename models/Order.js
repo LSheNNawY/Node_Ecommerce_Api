0c0f12ee4d841b67de7652b742aea78d41142283
sara-mohamed12 <78587095+sara-mohamed12@users.noreply.github.com>
2021-06-02 14:33:13 +0200

    just test

--- models/Order.js
+++ models/Order.js
@@ -1,54 +1,56 @@
-const mongoose = require('mongoose');
-const {Schema} = require("mongoose");
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+const Joi = require("joi");
 
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     user: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
+      type: Schema.Types.ObjectId,
+      ref: "User",
     },
     totalPrice: {
-        type: Number,
-        default: 0,
-        required: true
+      type: Number,
+      default: 0,
+      required: true,
     },
 
     products: [
-        {
-            product_id:
-                {
-                    type: Schema.Types.ObjectId,
-                    ref: "Product",
-                    required: true
-                },
-            quantity:
-                {
-                    type: Number,
-                    default: 1
+      {
+        product_id: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
 
-                },
-        }
-
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
     ],
 
-
     status: {
-        type: String,
-        enum: ['pending', 'accepted', 'rejected'],
-        default: "pending"
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
     },
-}, {timestamps: {createdAt: 'created_at', updatedAt: false}});
-const Order = mongoose.model('Order', orderSchema);
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: false,
+    },
+  }
+);
+const Order = mongoose.model("Order", orderSchema);
 
 function validateOrder(order) {
-    const schema = Joi.object({
-        totalPrice: Joi.number().required(),
-        products: Joi.required()
-    });
-    return schema.validate(order);
+  const schema = Joi.object({
+    totalPrice: Joi.number().required(),
+    products: Joi.required(),
+  });
+  return schema.validate(order);
 }
 
 module.exports.Order = Order;
 module.exports.validate = validateOrder;
-
-
